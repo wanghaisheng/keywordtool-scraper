41bbdc7988b0d42776ecd1d8bee40ecad74f0bf8
Hans Musgrave <hans.musgrave@gmail.com>
2018-01-11 22:25:20 -0600

    updated meta.yaml

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 setup(
-    name="HelloWorld",
-    version="0.0.0",
+    name="Keywordtool Scraper",
+    version="0.0.2",
     packages=find_packages(),
 )
